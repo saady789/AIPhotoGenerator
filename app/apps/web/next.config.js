ecf9f38a2fed00e8e1f116d56c7ee80662a664e7
saady <saadydev@gmail.com>
2025-10-12 19:49:17 -0400

    prisma error

--- app/apps/web/next.config.js
+++ app/apps/web/next.config.js
@@ -1,7 +1,15 @@
 /** @type {import('next').NextConfig} */
+import { PrismaPlugin } from "@prisma/nextjs-monorepo-workaround-plugin";
+
 const nextConfig = {
   images: {
     domains: ["r2-us-west.photoai.com"], // ✅ Add this line
+  },
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.plugins = [...config.plugins, new PrismaPlugin()];
+    }
+    return config;
   },
 };
 
